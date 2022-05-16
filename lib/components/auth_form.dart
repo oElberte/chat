a3b79a72ccb955c96e3bc5b874d4003c750e0b2b
Elberte Plínio <89710472+oElberte@users.noreply.github.com>
2022-05-16 19:48:38 -0300

    Image picker on auth screen

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,7 +1,48 @@
+import 'dart:io';
+
+import 'package:chat/components/user_image_picker.dart';
+import 'package:chat/core/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthForm extends StatelessWidget {
-  const AuthForm({Key? key}) : super(key: key);
+class AuthForm extends StatefulWidget {
+  final void Function(AuthFormData) onSubmit;
+
+  const AuthForm({
+    Key? key,
+    required this.onSubmit,
+  }) : super(key: key);
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _formData = AuthFormData();
+
+  void _handleImagePick(File image) {
+    _formData.image = image;
+  }
+
+  void _showError(String msg) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(msg),
+        backgroundColor: Theme.of(context).errorColor,
+      ),
+    );
+  }
+
+  void _submit() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+    if (!isValid) return;
+
+    if (_formData.image == null && _formData.isSignup) {
+      return _showError('Image not selected!');
+    }
+
+    widget.onSubmit(_formData);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,26 +51,70 @@ class AuthForm extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
+              if (_formData.isSignup)
+                UserImagePicker(
+                  onImagePick: _handleImagePick,
+                ),
+              if (_formData.isSignup)
+                TextFormField(
+                  key: const ValueKey('name'),
+                  initialValue: _formData.name,
+                  onChanged: (name) => _formData.name = name,
+                  decoration: const InputDecoration(labelText: 'Name'),
+                  validator: (_name) {
+                    final name = _name ?? '';
+                    if (name.trim().length < 5) {
+                      return 'The name must be at least 5 characters!';
+                    }
+                    return null;
+                  },
+                ),
               TextFormField(
-                decoration: const InputDecoration(labelText: 'Name'),
-              ),
-              TextFormField(
+                key: const ValueKey('email'),
+                initialValue: _formData.email,
+                onChanged: (email) => _formData.email = email,
                 decoration: const InputDecoration(labelText: 'E-mail'),
+                validator: (_email) {
+                  final email = _email ?? '';
+                  if (!email.contains('@')) {
+                    return 'Invalid e-mail';
+                  }
+                  return null;
+                },
               ),
               TextFormField(
                 obscureText: true,
+                key: const ValueKey('password'),
+                initialValue: _formData.password,
+                onChanged: (password) => _formData.password = password,
                 decoration: const InputDecoration(labelText: 'Password'),
+                validator: (_password) {
+                  final password = _password ?? '';
+                  if (password.length < 6) {
+                    return 'The password must be at least 6 characters!';
+                  }
+                  return null;
+                },
               ),
               const SizedBox(height: 12),
               ElevatedButton(
-                child: const Text('Sign-in'),
-                onPressed: () {},
+                child: Text(_formData.isLogin ? 'Sign-in' : 'Sign-up'),
+                onPressed: _submit,
               ),
               TextButton(
-                child: const Text('Create an account?'),
-                onPressed: () {},
+                child: Text(
+                  _formData.isLogin
+                      ? 'Create an account?'
+                      : 'Already have an account?',
+                ),
+                onPressed: () {
+                  setState(() {
+                    _formData.toggleAuthMode();
+                  });
+                },
               ),
             ],
           ),
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,17 +1,54 @@
 import 'package:chat/components/auth_form.dart';
+import 'package:chat/core/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  bool _isLoading = false;
+
+  Future<void> _handleSubmit(AuthFormData formData) async {
+    try {
+      setState(() => _isLoading = true);
+
+      if (formData.isLogin) {
+        // Login
+      } else {
+        // Signup
+      }
+    } catch (error) {
+      // Treat error
+    } finally {
+      setState(() => _isLoading = false);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: const Center(
-        child: SingleChildScrollView(
-          child: AuthForm(),
-        ),
+      body: Stack(
+        children: [
+          Center(
+            child: SingleChildScrollView(
+              child: AuthForm(onSubmit: _handleSubmit),
+            ),
+          ),
+          if (_isLoading)
+            Container(
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(0, 0, 0, 0.5),
+              ),
+              child: const Center(
+                child: CircularProgressIndicator.adaptive(),
+              ),
+            ),
+        ],
       ),
     );
   }
