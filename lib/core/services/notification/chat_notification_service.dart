dc7c889b0f03489eec38aecbee953aadda355f86
Elberte Plínio <89710472+oElberte@users.noreply.github.com>
2022-05-19 19:57:40 -0300

    Push notifications on Android

--- lib/core/services/notification/chat_notification_service.dart
+++ lib/core/services/notification/chat_notification_service.dart
@@ -1,4 +1,5 @@
 import 'package:chat/core/models/chat_notification.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 
 class ChatNotificationService with ChangeNotifier {
@@ -21,5 +22,49 @@ class ChatNotificationService with ChangeNotifier {
   void remove(int i) {
     _items.removeAt(i);
     notifyListeners();
+  }
+
+  // Push Notifications
+  Future<void> init() async {
+    await _configureTerminated();
+    await _configureForeground();
+    await _configureBackground();
+  }
+
+  Future<bool> get _isAuthorized async {
+    final messaging = FirebaseMessaging.instance;
+    final settings = await messaging.requestPermission();
+    return settings.authorizationStatus == AuthorizationStatus.authorized;
+  }
+
+  Future<void> _configureTerminated() async {
+    if (await _isAuthorized) {
+      RemoteMessage? initialMsg =
+          await FirebaseMessaging.instance.getInitialMessage();
+      _messageHandler(initialMsg);
+    }
+  }
+
+  Future<void> _configureForeground() async {
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessage.listen(_messageHandler);
+    }
+  }
+
+  Future<void> _configureBackground() async {
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessageOpenedApp.listen(_messageHandler);
+    }
+  }
+
+  void _messageHandler(RemoteMessage? msg) {
+    {
+      if (msg == null || msg.notification == null) return;
+
+      add(ChatNotification(
+        title: msg.notification!.title ?? 'Unknown!',
+        body: msg.notification!.body ?? 'Unknown!',
+      ));
+    }
   }
 }
